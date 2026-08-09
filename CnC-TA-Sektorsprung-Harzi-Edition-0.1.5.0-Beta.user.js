a62efe807b2dd88feb7a390e2e259dcb420200c3
Harzi66 <harzinger@t-online.de>
2026-08-09 09:57:17 +0200

    Rename script and update version format

--- CnC-TA-Sektorsprung-Harzi-Edition-0.1.5.0-Beta.user.js
+++ CnC-TA-Sektorsprung-Harzi-Edition-0.1.5.0-Beta.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name           ToolBox_Addon_Sektorsprung
+// @name           CnC-TA-Sektorsprung-Harzi-Edition
 // @author         Trinitroglycerol
 // @contributor    leo7044 (https://github.com/leo7044)
-// @version        0.1.5.0 Beta
+// @version        0.1.5.0-Beta
 // @description    Erweiterte Version des Toolbox_Addon_Sektorsprung
 // @description    Original by Trinitroglycerol, leo7044, further developed by Harzi66.
 // @description    Landepunkt für Spieler berechnen
